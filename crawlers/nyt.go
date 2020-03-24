b90fb6e1e0237a91819dcb3866be5f92a3e88c85
Niraj-Fonseka <fonseka.live@gmail.com>
2020-03-24 01:54:17 -0500

    getting ready to write the crawler

---
+++ crawlers/nyt.go
@@ -0,0 +1,8 @@
+package crawlers
+
+//https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html#g-cases-by-county
+func CrawlNYT() map[string]map[string]int {
+	dataStore := make(map[string]map[string]int)
+
+	return dataStore
+}
